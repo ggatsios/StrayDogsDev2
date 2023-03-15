86ae7688fa36cd171ef3a89f98664601569104d8
GatsiosGiorgos <37058751+ggatsios@users.noreply.github.com>
2023-03-15 15:56:01 +0200

    ReportDogActivity and layout

---
+++ app/src/main/java/com/example/straydogsdev/ReportDogActivity.java
@@ -0,0 +1,81 @@
+package com.example.straydogsdev;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ReportDogActivity extends AppCompatActivity {
+
+    private EditText editTextDogName;
+    private EditText editTextDogLocation;
+    private EditText editTextDogColor;
+    private EditText editTextDogBreed;
+
+    private EditText editTextDogGender;
+
+    private Button buttonReportDog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_dog);
+
+        editTextDogName = findViewById(R.id.editTextDogName);
+        editTextDogLocation = findViewById(R.id.editTextDogLocation);
+        editTextDogColor = findViewById(R.id.editTextDogColor);
+        editTextDogBreed = findViewById(R.id.editTextDogBreed);
+        editTextDogGender = findViewById(R.id.editTextDogGender);
+        buttonReportDog = findViewById(R.id.buttonReportDog);
+
+        buttonReportDog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextDogName.getText().toString().trim();
+                String breed = editTextDogBreed.getText().toString().trim();
+                String color = editTextDogColor.getText().toString().trim();
+                String location = editTextDogLocation.getText().toString().trim();
+
+                if (TextUtils.isEmpty(name)) {
+                    editTextDogName.setError("Please enter the dog's name");
+                    editTextDogName.requestFocus();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(breed)) {
+                    editTextDogBreed.setError("Please enter the dog's breed");
+                    editTextDogBreed.requestFocus();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(color)) {
+                    editTextDogColor.setError("Please enter the dog's color");
+                    editTextDogColor.requestFocus();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(location)) {
+                    editTextDogLocation.setError("Please enter the location");
+                    editTextDogLocation.requestFocus();
+                    return;
+                }
+
+                DatabaseReference databaseDogs = FirebaseDatabase.getInstance().getReference("dogs");
+                String id = databaseDogs.push().getKey();
+                StrayDogData dog = new StrayDogData(id, location, name, color, breed, gender, imageURL, description);
+                databaseDogs.child(id).setValue(dog);
+
+                Toast.makeText(ReportDogActivity.this, "Dog reported successfully", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+    }
+}
+
--- app/src/main/java/com/example/straydogsdev/StrayDogData.java
+++ app/src/main/java/com/example/straydogsdev/StrayDogData.java
@@ -3,8 +3,9 @@ package com.example.straydogsdev;
 public class StrayDogData {
     private String id;
     private String name;
+
+    private String color;
     private String breed; //It may be removed
-    private int age;
     private String gender;
     private String imageUrl;
     private String description;
@@ -14,11 +15,11 @@ public class StrayDogData {
 //        // Default constructor required for calls to DataSnapshot.getValue(DogModel.class)
 //    }
 
-    public StrayDogData(String id, String name, String breed, int age, String gender, String imageUrl, String description, String location, boolean isAdopted) {
+    public StrayDogData(String id, String name, String breed, String gender, String imageUrl, String description, String location) {
         this.id = id;
         this.name = name;
+        this.color = color;
         this.breed = breed;
-        this.age = age;
         this.gender = gender;
         this.imageUrl = imageUrl;
         this.description = description;
@@ -33,12 +34,13 @@ public class StrayDogData {
         return name;
     }
 
-    public String getBreed() {
-        return breed;
+    public String getColor(){
+        return color;
+
     }
 
-    public int getAge() {
-        return age;
+    public String getBreed() {
+        return breed;
     }
 
     public String getGender() {
@@ -66,12 +68,11 @@ public class StrayDogData {
         this.name = name;
     }
 
+    public void setColor(String color){
+        this.color = color;
+    }
     public void setBreed(String breed) {
         this.breed = breed;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public void setGender(String gender) {
@@ -98,8 +99,8 @@ public class StrayDogData {
         return "DogModel{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
+                ", color ='" + color + '\'' +
                 ", breed='" + breed + '\'' +
-                ", age=" + age +
                 ", gender='" + gender + '\'' +
                 ", imageUrl='" + imageUrl + '\'' +
                 ", description='" + description + '\'' +
